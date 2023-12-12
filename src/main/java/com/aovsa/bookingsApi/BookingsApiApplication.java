49b4a4a7d9620495eebd2c3b9f13efbb3cf07299
Alejandro Oller Valiente <aollerv@Alejandros-MacBook-Pro.local>
2023-12-12 20:48:55 +0100

    Initial commit.

---
+++ src/main/java/com/aovsa/bookingsApi/BookingsApiApplication.java
@@ -0,0 +1,11 @@
+package com.aovsa.bookingsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingsApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BookingsApiApplication.class, args);
+	}
+}
